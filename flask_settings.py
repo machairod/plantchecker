f46a431184c79b050a30dde4f7e235b285c80cd2
machairod <povilikina@gmail.com>
2021-09-21 16:40:05 +0300

    plantchecker methods and flask api updates

--- flask_settings.py
+++ flask_settings.py
@@ -1,8 +1,8 @@
-from flask import Flask, url_for, request, jsonify
+from flask import Flask, request, jsonify
 from plantchecker import Plantchecker
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-# create/delete login, create/delete plant, update water/fertile
 
 
 @app.route('/')
@@ -14,29 +14,50 @@ def index():
 def login(): pass
 
 
+@app.route('/users/')
+def add_user(): pass
+
+
+# create user plant with spec and last watering and last fertiling dates
+@app.route('/plants/', methods=['POST', 'DELETE'])
+def plants():
+    json = request.get_json()
+    if request.method == 'POST':
+        return Plantchecker.add_user_plant(json)
+    else:
+        pass
+
+
 # get json list of user's plants
-@app.route('/user/<username>/', methods=['GET'])
-def usercard(username):
-    return jsonify(Plantchecker.checkUserPlants(username))
+@app.route('/plants/list/', methods=['GET'])
+def usercard():
+    username = request.args.get('username',default = None, type = str)
+    id = request.args.get('user_id',default = None, type = int)
+    return jsonify(Plantchecker.check_user_plants(username,id))
 
 
-@app.route('/addwater/', methods=['PUT'])
-def addwater():
+# update last_watering date for user plant
+# json must contain at least 'name' and likely, a date, if not it would be today
+@app.route('/plants/water/', methods=['PUT'])
+def water():
     json = request.get_json()
     return Plantchecker.add_plant_water(json)
 
 
-
-@app.route('/calc/', methods=['POST'])
-def calc():
+# update last_watering date for user plant
+# json must contain at least 'name' and likely, a date, if not it would be today
+@app.route('/plants/fertilew/', methods=['PUT'])
+def fertile():
     json = request.get_json()
-    result = json['a']+json['b']
-    return f'Был получен {request.method} запрос. Результат - {result}\n'
-
+    return Plantchecker.add_plant_fertile(json)
 
 with app.test_request_context():
-    print("it's working")
+        print("it's working")
+        print(app.url_map)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000,debug=True)
+    with app.test_request_context():
+        print("it's working")
+        print(app.url_map)
--- plantchecker.py
+++ plantchecker.py
@@ -13,11 +13,8 @@ connection = connect(
     database=config['plantbase']['database']
 )
 
-
-# connection = connect(option_files=configfile)
-
 class Plantchecker():
-    def checkUserPlants(login: str):
+    def check_user_plants(login: str):
         global connection
         with connection.cursor() as cursor:
             cursor.execute('select id from users where login = "{login}"'.format(login=login))
@@ -50,13 +47,13 @@ class Plantchecker():
         cursor.close()
         return user_plants
 
-    def userPlantCard(plant_id: int = None, plantname: str = ''):
+    def user_plantcard(plant_id: int = None, plantname: str = ''):
         global connection
         with connection.cursor() as cursor:
             try:
                 cursor.execute(
                     "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'plantcheckerDB' AND TABLE_NAME = 'userplants';")
-                columnlist = [('').join(x) for x in cursor.fetchall()]
+                columnlist = [''.join(x) for x in cursor.fetchall()]
                 plant_card = {}
                 for x in columnlist:
                     cursor.execute(
@@ -69,7 +66,7 @@ class Plantchecker():
             except Error as e:
                 print(e)
 
-    def addUserPlant(login: str, plantname: str, plantspec: str, last_watering: str = None):
+    def add_user_plant(login: str, plantname: str, plantspec: str, last_watering: str = None):
         global connection
         plant_data = {}
         with open('plantspecies.json', 'r') as plantspecies:
@@ -80,10 +77,10 @@ class Plantchecker():
 
         if type(last_watering) == str and (len(last_watering) == 10) and last_watering.find('.') == 2:
             plant_data["last_watering"] = datetime.datetime.strptime(last_watering, "%d.%m.%Y").date()
-        elif last_watering == None:
+        elif last_watering is None:
             plant_data['last_watering'] = datetime.date.today()
         else:
-            return ('Неправильный формат даты. Используйте "dd.mm.yyyy".')
+            return 'Неправильный формат даты. Используйте "dd.mm.yyyy".\n'
 
         plant_data["last_fertiling"] = plant_data["last_watering"]
         plant_data['plantspec'] = plantspec
@@ -95,7 +92,11 @@ class Plantchecker():
         plant_data["spraying"] = plant_dict[plantspec]['spraying']
         plant_data["plantname"] = plantname
 
-        add_plant = "insert into userplants (user, plantname, plantspec, last_watering, last_fertiling, water_freq_summer, water_freq_winter, fertile_freq_summer, fertile_freq_winter, spraying, light) values (%(user)s, %(plantname)s, %(plantspec)s, %(last_watering)s, %(last_fertiling)s, %(water_freq_summer)s, %(water_freq_winter)s, %(fertile_freq_summer)s, %(fertile_freq_winter)s, %(spraying)s, %(light)s)"
+        add_plant = '''insert into userplants (user, plantname, plantspec, last_watering, 
+        last_fertiling, water_freq_summer, water_freq_winter, fertile_freq_summer,
+         fertile_freq_winter, spraying, light) values (%(user)s, %(plantname)s, %(plantspec)s,
+          %(last_watering)s, %(last_fertiling)s, %(water_freq_summer)s, %(water_freq_winter)s,
+           %(fertile_freq_summer)s, %(fertile_freq_winter)s, %(spraying)s, %(light)s)'''
 
         with connection.cursor() as cursor:
             cursor.execute('select id from users where login="{user}"'.format(user=login))
@@ -106,40 +107,37 @@ class Plantchecker():
 
             try:
                 cursor.execute(
-                    'select * from userplants where id = {id} and plantname = "{plantname}" and plantspec = "{plantspec}"'.format(
+                    'select * from userplants where id = {id} and plantname = "{plantname}"'
+                    ' and plantspec = "{plantspec}"'.format(
                         id=user[0][0], plantname=plantname, plantspec=plantspec))
                 checkdata = cursor.fetchall()
                 print(checkdata)
                 if len(checkdata) != 0:
                     print(checkdata)
-                    return ("Похоже, это растение вы уже вносили")
+                    return "Похоже, это растение вы уже добавили\n"
                 cursor.execute(add_plant, plant_data)
                 connection.commit()
-                return ('Растение добавлено в ваш список')
+                return 'Растение добавлено в ваш список'
             except Error as e:
                 print(e)
 
-    def add_plant_water(plantdata: json, **kwargs):
+    def add_plant_water(plantdata: json):
         global connection
         add_data = {}
         if 'date' in plantdata:
             date = plantdata['date']
             if type(date) == str and len(date) == 10 and date.find('.') == 2:
-                add_data['date'] = datetime.datetime.strptime(date, "%d.%m.%Y").date()
+                date = datetime.datetime.strptime(date, "%d.%m.%Y").date()
             else:
-                return 'Неправильный формат даты. Используйте строку "dd.mm.yyyy".'
+                return 'Неправильный формат даты. Используйте строку "dd.mm.yyyy".\n'
         else:
             date = datetime.date.today()
 
-        if 'name' in plantdata:
-            plant = plantdata['name']
-        else:
-            plant = None
-
-        plant_id = kwargs.get('id', None)
+        plant = plantdata['name'] if 'name' in plantdata else None
+        plant_id = plantdata['id'] if 'id' in plantdata else None
 
         if plant is None and plant_id is None:
-            return 'Не определили растение, которое вы полили'
+            return 'Не определили растение, которое вы полили\n'
 
         with connection.cursor() as cursor:
             cursor.execute(
@@ -153,89 +151,82 @@ class Plantchecker():
 
             plantsql = cursor.fetchall()
             this_month = date.month
-            if this_month in range(3, 10):
-                gap = plantsql[0][1]
-            else:
-                gap = plantsql[0][2]
+            gap = plantsql[0][1] if this_month in range(3, 10) else plantsql[0][2]
             add_data['next_date'] = date + datetime.timedelta(days=gap)
 
             try:
                 cursor.execute(add_string, add_data)
                 connection.commit()
                 cursor.close()
-                return 'Событие зафиксировано, растение {plant} полито\n'.format(plant=plant)
+                if plant is None:
+                    return 'Событие зафиксировано, растение id:{id} полито {date}\n'.format(id=plant_id, date=str(date))
+                return 'Событие зафиксировано, растение {plant} полито {date}\n'.format(plant=plant, date=str(date))
 
             except Error as e:
-                return 'Произошла ошибка - '+str(e)
+                return 'Произошла ошибка - '+str(e)+'\n'
 
-
-    def add_user_fertiling(last_fertiling: str = None, plant: str = None):
+    def add_plant_fertile(plantdata: json):
         global connection
         add_data = {}
+        if 'date' in plantdata:
+            date = plantdata['date']
+            if type(date) == str and len(date) == 10 and date.find('.') == 2:
+                date = datetime.datetime.strptime(date, "%d.%m.%Y").date()
 
-        if last_fertiling == None:
-            return ('Неправильная дата')
-        elif type(last_fertiling) == str and len(last_fertiling) == 10 and last_fertiling.find('.') == 2:
-            add_data['date'] = datetime.datetime.strptime(last_fertiling, "%d.%m.%Y").date()
+            else:
+                return 'Неправильный формат даты. Используйте строку "dd.mm.yyyy".\n'
         else:
-            return ('Неправильный формат даты. Используйте "dd.mm.yyyy".')
+            date = datetime.date.today()
 
-        if plant == None:
-            return ('Не определили растение, которое вы подкормили')
+        plant = plantdata['name'] if 'name' in plantdata else None
+        plant_id = plantdata['id'] if 'id' in plantdata else None
+
+        if plant is None and plant_id is None:
+            return 'Не определили растение, которое вы удобрили.\n'
 
         with connection.cursor() as cursor:
-
-            if plant.isdigit() == True:
-                plant_id = int(plant)
-                cursor.execute(
-                    'select last_fertiling,fertile_freq_summer, fertile_freq_winter from userplants where id = "{id}"'.format(
-                        id=plant_id))
-
-                add_data['id'] = plant_id
-                add_string = 'update userplants set last_fertiling = %(date)s, next_fertile= %(next_date)s where id = %(id)s'
-
-
-            # надо переписать чтобы работало на строки с включением цифр
-            elif plant.isalpha() == True:
-                plantname = plant
-                cursor.execute(
-                    'select last_fertiling,fertile_freq_summer, fertile_freq_winter from userplants where plantname="{plantname}"'.format(
-                        plantname=plantname))
-
-                add_data['plantname'] = plant
-                add_string = 'update userplants set last_fertiling = %(date)s, next_fertile= %(next_date)s where plantname = %(plantname)s'
+            cursor.execute(
+                '''select last_fertiling,fertile_freq_summer, fertile_freq_winter from userplants 
+                         where id = "{id}" or plantname = "{name}"'''.format(id=plant_id, name=plant))
+            add_data['id'] = plant_id
+            add_data['name'] = plant
+            add_data['date'] = date
+            add_string = '''update userplants set last_fertiling = %(date)s, 
+                                    next_fertile= %(next_date)s where id = %(id)s or plantname = %(name)s'''
 
             plantsql = cursor.fetchall()
-            if plantsql[0][0].month in range(3, 10):
-                add_data['next_date'] = plantsql[0][0] + datetime.timedelta(days=plantsql[0][1])
-            else:
-                add_data['next_date'] = plantsql[0][0] + datetime.timedelta(days=plantsql[0][2])
+            this_month = date.month
+            gap = plantsql[0][1] if this_month in range(3, 10) else plantsql[0][2]
+            add_data['next_date'] = date + datetime.timedelta(days=gap)
 
             try:
                 cursor.execute(add_string, add_data)
                 connection.commit()
-                return ('Событие зафиксировано, растение {plant} подкормлено'.format(plant=plant))
+                cursor.close()
+                if plant is None:
+                    return  'Событие зафиксировано, растение id:{id} удобрено {date}\n'.format(id=plant_id, date=str(date))
+                return 'Событие зафиксировано, растение {plant} удобрено {date}\n'.format(plant=plant, date=str(date))
 
             except Error as e:
-                print(e)
+                return 'Произошла ошибка - '+str(e)+'\n'
 
-    def deletePlant(login: str = None, plantname: str = None):
+    def delete_plant(login: str = None, plantname: str = None):
         global connection
         with connection.cursor() as cursor:
             cursor.execute('select id from users where login = "{login}"'.format(login=login))
             id = cursor.fetchone()
             if id is None:
-                return "Мы вас не узнали. Укажите ваш логин"
+                return "Мы вас не узнали. Укажите ваш логин\n"
             id = id[0]
             cursor.execute('select * from userplants where user="{id}"'.format(id=id))
             deleting_userplant = cursor.fetchall()
             if len(deleting_userplant) == 0:
-                return 'У вас еще нет растений'
+                return 'У вас еще нет растений\n'
             cursor.execute('delete from userplants where plantname = "{plantname}"'.format(plantname=plantname))
             connection.commit()
 
-    def addUser(login: str = None, platform: str = None):
+    def add_user(login: str = None, platform: str = None):
         pass
 
-    def deleteUser(login: str):
+    def delete_user(login: str):
         pass
